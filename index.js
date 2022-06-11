7936d5cdcc7ac3f1a9b7fb0da791a993fc4980e0
go.yokoyama <go.yokoyama@persol.co.jp>
2022-06-11 16:09:06 +0900

    First commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({extended:false}));
+
+app.get('/about', function (req, res) {
+  res.send('Aboutページ');
+});
+app.post('/api/v1/quiz', (req,res) => {
+  const answer = req.body.answer;
+  if (answer === "2") {
+    // res.send("<h1>正解！</h1>");
+    res.redirect("/correct.html")
+  } else {
+    // res.send("<h1><不正解！/h1>");
+    res.redirect("/wrong.html")
+  }
+});
+app.get('/api/v1/users', (req,res) => res.send({name:"Mike",age:30}));
+app.listen(3000,() => console.log("I'm running!"));
+
+console.log("最終行");
